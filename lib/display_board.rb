59032e06364747a0af8bfa5c251e154554d1403a
Harrison Price <hprice518@gmail.com>
2020-10-04 02:27:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,6 @@
 
 board = [" "," "," "," ","X"," "," "," "," ",]
 
-#def display_board(board)
-#  puts "#{board[0]}, #{board[0]}, #{board[0]}, #{board[0]}, #{board[0]}, #{board[0]},# #{board[0]}, #{board[0]}, #{board[0]}, #{board[0]}"
-#end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -15,3 +11,4 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
   
+display_board(board)
